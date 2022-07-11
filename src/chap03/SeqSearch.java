e2e1a02a073f6465ac9fb7a358c70eff988780b1
임채성 <pan1129@naver.com>
2022-07-11 12:44:09 +0900

    seqSearch 수정 Commit

--- src/chap03/SeqSearch.java
+++ src/chap03/SeqSearch.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class SeqSearch {
 
-    static int seqSearch(int[] a, int n, int key){
-        int i = 0;
+//    static int seqSearch(int[] a, int n, int key){
+//        int i = 0;
+//
+//        while (true){
+//            if(i == n)
+//                return -1;
+//            if(a[i] == key)
+//                return i;
+//            i++;
+//        }
+//    }
 
-        while (true){
-            if(i == n)
-                return -1;
+    static int seqSearch(int[] a, int n, int key){
+        for(int i = 0; i < n; i++)
             if(a[i] == key)
                 return i;
-            i++;
-        }
-    }
+            return -1;
 
+    }
 
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
